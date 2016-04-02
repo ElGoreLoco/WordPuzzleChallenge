3f10bf4acb85d8e93d36e64df2caee5c24426a64
ElGoreLoco <esegoreloco@gmail.com>
2016-04-02 20:27:30 +0200

    Begin IU and keyboard input

--- main.c
+++ main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 
 void getPuzzle(char *puzzle);
-void printPuzzle(char *puzzle);
+void printPuzzle(char *puzzle, char x, char y);
 
 int main()
 {
+    int c;
+    char x, y;
+    x = y = 0;
+
     char puzzle[50][50];
     char *ppuzzle = &(puzzle[0][0]);
     getPuzzle(ppuzzle);
-    printPuzzle(ppuzzle);
+    printPuzzle(ppuzzle, x, y);
+
+    while ((c = getchar()) != 'q') {
+        printPuzzle(ppuzzle, x, y);
+        switch (c) {
+            case 'w':
+                if (y > 0)
+                    y--;
+                break;
+            case 'a':
+                if (x > 0)
+                    x--;
+                break;
+            case 's':
+                if (y < 49)
+                    y++;
+                break;
+            case 'd':
+                if (x < 49)
+                    x++;
+                break;
+        }
+    }
 
     return 0;
 }
@@ -38,14 +64,18 @@ void getPuzzle(char *puzzle)
     fclose(file);
 }
 
-void printPuzzle(char *puzzle)
+void printPuzzle(char *puzzle, char x, char y)
 {
     int i, j;
 
     // Print puzzle
+    system("clear");
     for (i = 0; i < 50; ++i) {
         for (j = 0; j < 50; ++j)
-            printf("%c ", puzzle[i*50+j]);
+            if (i == y && j == x)
+                printf("\x1b[31m%c\x1b[0m ", puzzle[i*50+j]);
+            else
+                printf("%c ", puzzle[i*50+j]);
         printf("\n");
     }
 }
